556d503dd4b908c6b19904acd9d40179d49a5614
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-12-29 13:58:54 +0800

    updated main.ts and added api versioning and global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { yap } from './utils/messages';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const PORT = parseInt(process.env.PORT, 10) || 8080;
@@ -20,6 +21,13 @@ async function bootstrap() {
     ],
     credentials: true,
   });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.setGlobalPrefix('api');
 
   await app
     .listen(PORT, '0.0.0.0', () => yap(`Server started at port ${PORT}`))
