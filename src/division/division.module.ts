f1a0a10783cf356a7bf47b2f3b33c3e6a0093041
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2025-01-16 11:36:50 +0800

    bug fix

--- src/division/division.module.ts
+++ src/division/division.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DivisionService } from './division.service';
 import { DivisionController } from './division.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [DivisionController],
-  providers: [DivisionService],
+  providers: [DivisionService, PrismaService],
 })
 export class DivisionModule {}
