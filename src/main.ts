05c995fbc2edf8571e1c87d570001adb0d73d2cd
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2025-01-17 16:17:28 +0800

    edited allowed header

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173/'],
+    origin: ['http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders:
       'Origin, Content-Type, Authorization, X-Requested-With, Cache-Control, x-api-key',
